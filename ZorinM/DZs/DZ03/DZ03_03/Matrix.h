4bd8c44e01fa502f1ad19d73968b10cd5441ceab
MykhailoZorin <mykhailo.zorin@gmail.com>
2013-08-10 12:51:55 +0300

    3-3. Matrix<>

--- ZorinM/DZs/DZ03/DZ03_03/Matrix.h
+++ ZorinM/DZs/DZ03/DZ03_03/Matrix.h
@@ -3,8 +3,10 @@
 
 #include <iostream>
 #include <iomanip>
+
 using std::cout;
 using std::ostream;
+using std::cerr;
 
 template <class T>
 class Matrix
@@ -13,24 +15,94 @@ class Matrix
 	T** matrix;
 
 public:
-	Matrix (int r, int c)
-	{
- 		rows = r;
- 		cols = c;
- 
- 		matrix = new T* [rows];
- 
- 		for ( int i = 0; i < rows; i++)
- 		{
- 			matrix[i] = new T [cols];
- 
- 			for (int j = 0; j < cols; j++)
- 				matrix[i][j] = (T)j;
+	Matrix ();
+	Matrix (int, int);
+	Matrix(const Matrix&);
+	~Matrix();
 
-		}
-	}
 	void show () const;
+	void fill();
+	void fill( T );
+	void set_rows ( int );
+	void set_cols ( int );
+
+	Matrix& operator = (const Matrix& );
+	T* Matrix::operator []( int ) const;
+	Matrix operator + ( const Matrix& );
+	Matrix& operator += ( const Matrix& );
+	Matrix operator - ( const Matrix& );
+	Matrix& operator -= ( const Matrix& );
+	Matrix operator * ( const Matrix& );
+	Matrix& operator *= ( const Matrix& );
+	Matrix& operator ~ ();
+
+	friend ostream& operator << ( ostream& output, const Matrix& m )
+	{
+		for ( int i = 0; i < m.rows; i++)
+		{
+			for ( int j = 0; j < m.cols; j++)
+			{
+				output.fill(' ');
+				output.width(7);
+				output << m.matrix[i][j];
+			}
+			output << "\n";
+		}
+	
+		return output;
+	}
+
 };
+
+template <class T>
+Matrix<T>::Matrix() : rows(0), cols(0), matrix(0)
+{}
+
+template <class T>
+Matrix<T>::Matrix (int r, int c)
+{
+ 	rows = r;
+ 	cols = c;
+	
+ 	matrix = new T* [rows];
+ 
+ 	for ( int i = 0; i < rows; i++)
+ 	{
+ 		matrix[i] = new T [cols];
+ 
+ 		for (int j = 0; j < cols; j++)
+ 			matrix[i][j] = (T)(rand()%99);
+
+	}
+}
+
+template <class T>
+Matrix<T>::Matrix(const Matrix& m)
+{
+	rows = m.rows;
+	cols = m.cols;
+
+	matrix = new T* [rows];
+	for ( int i = 0; i < rows; i++ )
+		matrix[i] = new T [cols];
+
+	for ( int i = 0; i < rows; i++ )
+		for ( int j = 0; j < cols; j++ )
+			matrix[i][j] = m.matrix[i][j];
+}
+
+template <class T>
+Matrix<T>::~Matrix()
+{
+	for (int i = 0; i < rows; i++)
+		delete [] matrix [i];
+
+	delete [] matrix;
+
+	rows = 0;
+	cols = 0;
+}
+
 
 template <class T>
 void Matrix<T>::show () const
@@ -39,12 +111,243 @@ void Matrix<T>::show () const
 	{
 		for ( int j = 0; j < cols; j++)
 		{
-			cout << matrix[i][j];
 			cout << std::setw(7) << matrix[i][j];
 		}
 		cout << "\n";
 	}
 }
 
+template <class T>
+void Matrix<T>::fill()
+{
+	for ( int i = 0; i < rows; i ++ )
+		for ( int j = 0; j < cols; j++)
+			matrix[i][j] = static_cast<T>(rand() % 99);
+}
+
+template <class T>
+void Matrix<T>::fill( T t)
+{
+	for ( int i = 0; i < rows; i ++ )
+		for ( int j = 0; j < cols; j++)
+			matrix[i][j] = t;
+}
+
+template <class T>
+void Matrix<T>::set_rows ( int r )
+{
+	for ( int i = 0; i < rows; i++ )
+		delete [] matrix [i];
+	delete [] matrix;
+
+	rows = r;
+	matrix = new T* [rows];
+
+	for ( int i = 0; i < rows; i++)
+		matrix [i] = new T [cols];
+
+	this->fill((T)99);
+	// this->fill((T)0);
+}
+
+template <class T>
+void Matrix<T>::set_cols ( int c )
+{
+	for ( int i = 0; i < rows; i++ )
+		delete [] matrix [i];
+	delete [] matrix;
+
+	cols = c;
+
+	matrix = new T* [rows];
+	for ( int i = 0; i < rows; i++ )
+		matrix[i] = new T [cols];
+
+	// this->fill((T)0);
+	this->fill((T)99);
+}
+
+template <class T>
+Matrix<T>& Matrix<T>::operator = (const Matrix<T>& m)
+{
+	if ( cols != m.cols || rows != m.rows)
+	{
+		for (int i = 0; i < rows; i++)
+			delete [] matrix [i];
+
+		delete [] matrix;
+
+		rows = m.rows;
+		cols = m.cols;
+
+		matrix = new T* [rows];
+		for (int i = 0; i < rows; i++)
+			matrix[i] = new T [cols];
+	}		
+
+	for ( int i = 0; i < rows; i++ )
+		for ( int j = 0; j < cols; j++ )
+			matrix[i][j] = m.matrix[i][j];
+
+	return *this;
+}
+
+template <class T>
+T* Matrix<T>::operator []( int i ) const
+{
+	return matrix[i];
+}
+
+template <class T>
+Matrix<T> Matrix<T>::operator + ( const Matrix<T>& m)
+{
+	if ( cols != m.cols || rows != m.rows)
+	{
+		cerr << "ERROR! Columns or rows has different size\n";
+		exit (1);
+	}
+
+	Matrix res( rows, cols );
+
+	for ( int i = 0; i < rows; i++)
+		for ( int j = 0; j < cols; j++ )
+			res.matrix[i][j] = matrix[i][j] + m.matrix[i][j];
+
+	return res;
+
+}
+
+template <class T>
+Matrix<T>& Matrix<T>::operator += ( const Matrix<T>& m )
+{
+	if ( cols != m.cols || rows != m.rows)
+	{
+		cerr << "ERROR! Columns or rows has different size\n";
+		exit (1);
+	}
+	
+	for ( int i = 0; i < rows; i++)
+		for ( int j = 0; j < cols; j++ )
+			matrix[i][j] += m.matrix[i][j];
+
+	return *this;
+}
+
+template <class T>
+Matrix<T> Matrix<T>::operator - ( const Matrix<T>& m )
+{
+	if ( cols != m.cols || rows != m.rows)
+	{
+		cout << "ERROR! Columns or rows has different size\n";
+		exit (1);
+	}
+
+	Matrix res( rows, cols );
+
+	for ( int i = 0; i < rows; i++)
+		for ( int j = 0; j < cols; j++ )
+			res.matrix[i][j] = matrix[i][j] - m.matrix[i][j];
+
+	return res;
+}
+
+template <class T>
+Matrix<T>& Matrix<T>::operator -= ( const Matrix<T>& m )
+{
+	if ( cols != m.cols || rows != m.rows)
+	{
+		cerr << "ERROR! Columns or rows has different size\n";
+		exit (1);
+	}
+	
+	for ( int i = 0; i < rows; i++)
+		for ( int j = 0; j < cols; j++ )
+			matrix[i][j] -= m.matrix[i][j];
+
+	return *this;
+}
+
+template <class T>
+Matrix<T> Matrix<T>::operator * ( const Matrix<T>& m )
+{
+	if(cols == m.rows)
+	{
+		Matrix res(rows, m.cols);
+		for(int i = 0; i < res.rows; i++)
+		{
+			for (int j = 0; j < res.cols; j++)
+			{
+				res.matrix[i][j] = 0;
+				for(int x = 0; x < cols; x++)
+				{
+					res.matrix[i][j] += matrix[i][x] * m.matrix[x][j];
+				}
+			}
+		}
+		return res;
+	}
+
+	cerr << "ERROR! Rows first matrix doesn't equal Cols of second\n";
+	exit (1);	
+}
+
+template <class T>
+Matrix<T>& Matrix<T>::operator *= ( const Matrix<T>& m )
+{
+	if(cols == m.rows)
+	{
+		Matrix res(rows, m.cols);
+		for(int i = 0; i < res.rows; i++)
+		{
+			for (int j = 0; j < res.cols; j++)
+			{
+				res.matrix[i][j] = 0;
+				for(int x = 0; x < cols; x++)
+				{
+					res.matrix[i][j] += matrix[i][x] * m.matrix[x][j];
+				}
+			}
+		}
+		return (*this = res);
+	}
+
+	cerr << "ERROR! Rows first matrix doesn't equal Cols of second\n";
+	exit (1);	
+	
+}
+
+template <class T>
+Matrix<T>& Matrix<T>::operator ~ ()
+{
+	Matrix temp(rows, cols);
+
+	for ( int i = 0; i < rows; i++)
+	{
+		for ( int j = 0; j < cols; j++)
+		{
+			temp.matrix[i][j] = matrix[i][j];
+			// cout << temp.matrix[i][j] << " ";
+		}
+		// cout << "\n";
+	}
+
+	rows = temp.cols;
+	cols = temp.rows;
+
+	matrix = new T* [rows];
+	for ( int i = 0; i < rows; i++ )
+		matrix[i] = new T [cols];
+
+	for ( int i = 0; i < rows; i++ )
+	{
+		for ( int j = 0; j < cols; j++ )
+		{
+	 		matrix[i][j] = temp.matrix[j][i];
+		}
+	}
+
+	return *this;
+
+}
 
 #endif
--- ZorinM/DZs/DZ03/DZ03_03/main.cpp
+++ ZorinM/DZs/DZ03/DZ03_03/main.cpp
@@ -7,81 +7,83 @@ using std::cout;
 
 int main()
 {
-	//srand(unsigned(time(0)));
+	srand(unsigned(time(0)));
 
-	Matrix <float> mtrx1 (3, 5);
+	// Matrix <int> mtrx1;
+	Matrix <int> mtrx1 (3, 5);
 	mtrx1.show();
-	
-	// cout << "\n------------------------------------------------\n";
-	// mtrx1.fill(7);
-	// mtrx1.show();
-	// 
-	// cout << "\n------------------------------------------------\n";
-	// cout << "mtrx1.show()\n";
-	// mtrx1.fill();
-	// mtrx1.show();
 
-	// cout << "\n------------------------------------------------\n";
-	// cout << "mtrx2.show()\n";
-	// Matrix<int> mtrx2 = mtrx1;
-	// mtrx2.show();
-	// 
-	// cout << "\n------------------------------------------------\n";
-	// cout << "mtrx1[1][1] = ";
-	// cout << mtrx1[1][1];
-	// 
-	// cout << "\n------------------------------------------------\n";
-	// cout << "mtrx3 = mtrx1 + mtrx2\n";
-	// Matrix mtrx3 = mtrx1 + mtrx2;
-	// mtrx3.show();
-	// 
-	// cout << "\n------------------------------------------------\n";
-	// cout << "mtrx3 = mtrx3 - mtrx2 and \"<<\" overloaded operator\n";
-	// mtrx3 = mtrx3 - mtrx2;
-	// cout << mtrx3;
-	// 
-	// cout << "\n------------------------------------------------\n";
-	// cout << "cout << ^mtrx3\n";
-	// ~mtrx3;
-	// cout << mtrx3;
-	// 
-	// cout << "\n------------------------------------------------\n";
-	// cout << "set cols\n";
-	// mtrx3.set_cols(10);
-	// cout << mtrx3;
-	// 
-	// cout << "\n------------------------------------------------\n";
-	// cout << "set rows\n";
-	// mtrx3.set_rows(10);
-	// cout << mtrx3;
-	// 
-	// cout << "\n------------------------------------------------\n";
-	// Matrix mtrx4 (3, 5);
-	// mtrx4.fill();
-	// cout << mtrx4 << "\n+\n";
-	// cout << mtrx1 << "\n=\n";
-	// mtrx4 += mtrx1;
-	// cout << mtrx4;
-	// 
-	// cout << "\n------------------------------------------------\n";
-	// cout << mtrx4 << "\n-\n";
-	// cout << mtrx1 << "\n=\n";
-	// mtrx4 -= mtrx1;
-	// cout << mtrx4;
-	// 
-	// cout << "\n------------------------------------------------\n";
-	// Matrix mtrx5 (5, 3);
-	// mtrx5.fill();
-	// cout << mtrx5 << "\n*\n";
-	// cout << mtrx1 << "\n=\n";
-	// cout << mtrx5 * mtrx1;
-	// 
-	// cout << "\n------------------------------------------------\n";
-	// cout << "\n mtrx5 *= mtrx2 \n";
-	// mtrx5 *= mtrx2;
-	// mtrx5.show();
+	cout << "\n------------------------------------------------\n";
+	Matrix <int> mtrx2 = mtrx1;
+	mtrx2.show();
+
+	cout << "\n------------------------------------------------\n";
+	mtrx2.fill();
+	mtrx2.show();
+
+	cout << "\n------------------------------------------------\n";
+	mtrx2.fill('t');
+	mtrx2.show();
+
+	cout << "\n------------------------------------------------\n";
+	mtrx2.set_rows(5);
+	mtrx2.show();
+
+	cout << "\n------------------------------------------------\n";
+	mtrx2.set_cols(6);
+	mtrx2.show();
+
+	cout << "\n------------------------------------------------\n";
+	mtrx2 = mtrx1;
+	mtrx2.show();
+
+	cout << "\n------------------------------------------------\n";
+	cout << mtrx2[1][1];
+
+
+	cout << "\n------------------------------------------------\n";
+	Matrix <int> mtrx3 (3, 5);
+	Matrix <int> mtrx4;
+
+	mtrx4 = mtrx1 + mtrx3;
+	mtrx4.show();
+
+	cout << "\n------------------------------------------------\n";
+	mtrx4 += mtrx1;
+	mtrx4.show();
+
+	cout << "\n------------------------------------------------\n";
+	mtrx3 = mtrx4 - mtrx1;
+	mtrx3.show();
+
+	cout << "\n------------------------------------------------\n";
+	mtrx4 -= mtrx1;
+	mtrx4.show();
+
+	cout << "\n------------------------------------------------\n";
+	Matrix <int> mtrx5 (5, 3);
+	cout << "\n------------------------------------------------\n";
+	mtrx4.show();
+	cout << "\n";
+	mtrx5.show();
+	cout << "\n";
+
+	mtrx1 = mtrx4 * mtrx5;
+	mtrx1.show();
+
+	cout << "\n------------------------------------------------\n";
+	mtrx4 *= mtrx5;
+	mtrx4.show();
+
+	cout << "\n------------------------------------------------\n";
+	cout << mtrx4;
+
+	cout << "\n------------------------------------------------\n";
+	cout << mtrx2;
+
+	cout << "\n~\n";
+	cout << ~mtrx2;
 
 	cout << "\n\n";
-
 	return 0;
 }
