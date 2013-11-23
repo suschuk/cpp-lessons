fe28e1654ca707d92f0c3eb8c1d9ee9195063521
unknown <zhakhalov@gmail.com>
2013-11-24 00:46:30 +0200

    Form validation added

--- Zhakhalov/Javascript/Page/script.js
+++ Zhakhalov/Javascript/Page/script.js
@@ -1,85 +1,135 @@
-/* TEMPORARY DATA*/
+var menu = [];
+var tabs = [];
 
-var tabItems = [
-    {name:"tab-item1", "link":"info/1.html"},
-    {name:"tab-item2", "link":"info/2.html"},
-    {name:"tab-item3", "link":"info/3.html"},
-    {name:"tab-item4", "link":"info/4.html"},
-    {name:"tab-item5", "link":"info/5.html"}];
+var trueName = "_monolit@ss.comZx_monolith";
 
-var menuItems = [
-    {name:"menu-item1",
-        sub:[
-            {name:"sub1-item1","link":""},
-            {name:"sub1-item2","link":""},
-            {name:"sub1-item3","link":""},
-            {name:"sub1-item4","link":""}]},
-    {name:"menu-item2",
-        sub:[
-            {name:"sub2-item1","link":""},
-            {name:"sub2-item2","link":""},
-            {name:"sub2-item3","link":""},
-            {name:"sub2-item4","link":""},
-            {name:"sub2-item5","link":""},
-            {name:"sub2-item6","link":""}]},
-    {name:"menu-iem3",
-        sub:[
-            {name:"sub3-item1","link":""},
-            {name:"sub3-item3","link":""},
-            {name:"sub3-item4","link":""}]},
-    {name:"menu-item4",
-        sub:[
-            {name:"sub4-item1","link":""},
-            {name:"sub4-item4","link":""}]
-    }];
+var username = "";
+var password= "";
 
-var data = [
-    {text:"111111111111111111111111111111111111111111111111111<p>1111111111111111111111111111"},
-    {text:"222222222222222222222222222222222222222222222222222<p>1111111111111111111111111111"},
-    {text:"333333333333333333333333333333333333333333333333333<p>1111111111111111111111111111"},
-    {text:"444444444444444444444444444444444444444444444444444<p>1111111111111111111111111111"},
-    {text:"555555555555555555555555555555555555555555555555555<p>1111111111111111111111111111"}];
-
-
-/* TEMPORARY DATA*/
 $('document').ready(function()
-{
-    /*$.ajax({
+{	
+    $.ajax({
         type: "GET",
-        url: "menu.xml",
+        url: "config.xml",
         dataType: "xml",
-        success: parseTabs
-    });*/
+        success: parseXML
+    });
 
-    createMenu();
-    createTabs();
+    $("#user-name").keyup(function()
+    {
+        username = $("#user-name").val();
+    });
+
+    $("#user-password").keyup(function()
+    {
+        password = $("#user-password").val();
+        console.trace(password);
+    });
+    $("#login-button").click(function()
+    {
+        var wrong = "";
+        if((username.length < 5) || (username.length > 15))
+        {
+            wrong += " Username must contains 5-15 symbols!";
+            $("#user-name").removeClass();
+            $("#user-name").addClass("bad-text");
+        }
+        else if(!/^\w+$/.test(username))
+        {
+            $("#user-name").removeClass();
+            $("#user-name").addClass("bad-text");
+            wrong += " Username must contain only letters, numbers and underscores!";
+        }
+        else
+        {
+            $("#user-name").removeClass();
+            $("#user-name").addClass("good-text");
+        }
+
+        if((password.length < 5) || (password.length > 15))
+        {
+            wrong += " Password must contains 5-15 symbols!";
+            $("#user-password").removeClass();
+            $("#user-password").addClass("bad-text");
+        }
+        else if(!/^\w+$/.test(password))
+        {
+            $("#user-password").removeClass();
+            $("#user-password").addClass("bad-text");
+            wrong += " Password must contain only letters, numbers and underscores!";
+        }
+        else
+        {
+            $("#user-password").removeClass();
+            $("#user-password").addClass("good-text");
+        }
+
+        $("#user-hint").text(wrong);
+    });
 });
 
-/*function parseTabs(xml)
+function parseTabs(xml)
 {
-    $(xml).find("tabs").each(function()
+    $(xml).find("tab-item").each(function()
     {
-        var name = $(this).find("name").text();
-        tabItems.push(name);
+        var item = {title : "",link: ""};
 
-        var link = $(this).find("link").text();
-        tabLinks.push(link);
+        $(this).find("tab-title").each(function(){item.title = $(this).text();});
+        $(this).find("tab-link").each(function(){item.link = $(this).text();});
+
+        tabs.push(item);
     });
-    console.trace('tab parsed');
-}*/
+    console.trace('tabs parsed');
+    createTabs();
+}
+
+function parseXML(xml)
+{
+    console.trace("config received");
+    $("title").text($(xml).find("page-title").text());
+    parseMenu(xml);
+    parseTabs(xml);
+}
+
+function parseMenu(xml)
+{
+    $(xml).find("menu-item").each(function()
+    {
+        var item = {title : "", sub : new Array(), onclick : ""};
+
+
+        $(this).find("menu-title").each(function(){item.title = $(this).text();});
+        $(this).find("menu-onclick").each(function(){item.onclick = $(this).text();});
+
+        $(this).find("sub-menu").each(function()
+        {
+            $(this).find("sub-menu-item").each(function()
+            {
+                var subItem = {title : "", onclick : "", onhover : ""};
+                $(this).find("title").each(function(){subItem.title = $(this).text();});
+                $(this).find("onclick").each(function(){subItem.onclick = $(this).text();});
+                $(this).find("onhover").each(function(){subItem.onhover = $(this).text();});
+                item.sub.push(subItem);
+            });
+        });
+        menu.push(item);
+    });
+    console.trace('menu parsed');
+    createMenu();
+}
 
 function createMenu()
 {
-    for(var i = 0; i < menuItems.length; ++i)
+    for(var i = 0; i < menu.length; ++i)
     {
         var text = "<li class='menu-item'>";
-        text += menuItems[i].name;
+        text += menu[i].title;
         text += "<ul class='sub-menu'>";
 
-        for(var j = 0; j < menuItems[i].sub.length; ++j)
+        for(var j = 0; j < menu[i].sub.length; ++j)
         {
             text += "<li class='sub-menu-item'>";
-            text += menuItems[i].sub[j].name;
+            text += menu[i].sub[j].title;
             text += "</li>";
         }
 
@@ -88,7 +138,7 @@ function createMenu()
         $("#menu-list").append(text);
     }
 
-    $('.menu-item').css("width", (100 / menuItems.length) + "%");
+    $('.menu-item').css("width", (100 / menu.length) + "%");
 
     $(".menu-item").hover(
         function() {
@@ -114,18 +164,27 @@ function createMenu()
     {
         $(this).siblings().removeClass("sub-menu-item-hover");
         $(this).addClass("sub-menu-item-hover");
+		
+		var index = $(this).parent().index();
+		var subIndex = $(this).index();
+		// eval(menu[index].sub[subIndex]onhover);  
     }).mouseout(function()
     {
         $(this).parent().children().removeClass("sub-menu-item-hover");
-    });
+    }).click(function()
+	{
+		var index = $(this).parent().parent().index();
+		var subIndex = $(this).index();
+		eval(menu[index].sub[subIndex].onclick);  
+	});
 }
 
 function createTabs()
 {
-    for(var i = 0; i < tabItems.length; ++i)
-        $("#tab-list").append("<li class='tab-item tab-close'>" + tabItems[i].name + "</li>");
+    for(var i = 0; i < tabs.length; ++i)
+        $("#tab-list").append("<li class='tab-item tab-close'>" + tabs[i].title + "</li>");
 
-    $(".tab-item").css("width", (100 / tabItems.length) + "%");
+    $(".tab-item").css("width", (100 / tabs.length) + "%");
 
     $(".tab-item").mouseover(function()
     {
@@ -150,6 +209,6 @@ function createTabs()
 
 function openTab(idx)
 {
-//   $("#container").load(tabItems[idx].link);
-    $("#container").html(data[idx].text);
+    $("#container").load(tabs[idx].link);
 }
+
